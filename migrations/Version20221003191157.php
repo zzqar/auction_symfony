ba2f45a9736cfa5fcc9047ee52586f356461d1a6
zzqar <zzqar@vk.com>
2022-10-03 22:12:39 +0300

    paulloft  - Невозможно развернуть новый проект из-за некорректных миграций.

---
+++ migrations/Version20221003191157.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221003191157 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE goods (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, cost VARCHAR(255) NOT NULL, date_create DATE NOT NULL, costmax VARCHAR(255) NOT NULL, images VARCHAR(255) DEFAULT NULL, last_date DATE DEFAULT NULL, status VARCHAR(255) DEFAULT NULL, INDEX IDX_563B92DA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE transaction (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, good_id_id INT NOT NULL, pay VARCHAR(255) DEFAULT NULL, date DATETIME NOT NULL, status TINYINT(1) NOT NULL, INDEX IDX_723705D19D86650F (user_id_id), INDEX IDX_723705D161E73083 (good_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, balance VARCHAR(255) NOT NULL, vir_balance VARCHAR(255) NOT NULL, name VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE goods ADD CONSTRAINT FK_563B92DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D19D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D161E73083 FOREIGN KEY (good_id_id) REFERENCES goods (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE transaction DROP FOREIGN KEY FK_723705D161E73083');
+        $this->addSql('ALTER TABLE goods DROP FOREIGN KEY FK_563B92DA76ED395');
+        $this->addSql('ALTER TABLE transaction DROP FOREIGN KEY FK_723705D19D86650F');
+        $this->addSql('DROP TABLE goods');
+        $this->addSql('DROP TABLE transaction');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
